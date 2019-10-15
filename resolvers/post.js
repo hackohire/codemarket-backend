0f68439e03ff4cdbb158cd468feec1b22e115de1
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-10-15 20:25:14 +0530

    created separate file "event" for the lambdas functions related to event

--- resolvers/post.js
+++ resolvers/post.js
@@ -50,7 +50,7 @@ async function addPost(_, { post }, { headers, db, decodedToken }) {
             return reject(e);
         }
     });
-};
+}
 
 async function getPostsByUserIdAndType(_, { userId, status, postType }, { headers, db, decodedToken }) {
     return new Promise(async (resolve, reject) => {
@@ -176,103 +176,7 @@ async function updatePost(_, { post }, { headers, db, decodedToken }) {
     });
 }
 
-async function rsvpEvent(_, { userId, eventId }, { headers, db, decodedToken }) {
-    return new Promise(async (resolve, reject) => {
-        try {
 
-            if (!db) {
-                console.log('Creating new mongoose connection.');
-                conn = await connectToMongoDB();
-            } else {
-                console.log('Using existing mongoose connection.');
-            }
-
-            const sub = await Subscription.findOne({ "metadata.userId": { $eq: ObjectID(userId) } });
-            let validSubscription = false;
-            let updatedPostWithAttendees;
-
-            if (sub) {
-                validSubscription = moment(moment()).isBefore(sub.current_period_end * 1000, "YYYY/MM/DD");
-            }
-
-            if (validSubscription) {
-                updatedPostWithAttendees = await Post.findOneAndUpdate({ _id: eventId }, { $push: { usersAttending: userId } }, { new: true })
-                    .populate({ path: 'usersAttending' }).exec();
-            }
-
-            if (updatedPostWithAttendees) {
-                /** Send Email on Successful RSVP */
-                const filePath = basePath + 'email-template/rsvpSuccessful';
-                var eventLink = process.env.FRONT_END_URL + `(main:dashboard/${updatedPostWithAttendees.type}-details/${updatedPostWithAttendees._id})?type=${updatedPostWithAttendees.type}&postId=${updatedPostWithAttendees._id}`;
-                var attendee = updatedPostWithAttendees.usersAttending.find(u => u.id === userId);
-                const payLoad = {
-                    ATTENDEE: attendee.name,
-                    EVENT_NAME: updatedPostWithAttendees.name,
-                    EVENTLINK: eventLink,
-                    FROM: moment(updatedPostWithAttendees.dateRange[0]).format('dddd MMMM Do YYYY, h:mm a'),
-                    TO: moment(updatedPostWithAttendees.dateRange[1]).format('dddd MMMM Do YYYY, h:mm a'),
-                    ADDRESS: updatedPostWithAttendees.address ?  updatedPostWithAttendees.address : ''
-                };
-                await helper.sendEmail(attendee.email, filePath, payLoad);
-                return resolve({ usersAttending: updatedPostWithAttendees.usersAttending, validSubscription });
-            } else {
-                return resolve({ validSubscription })
-            }
-        } catch (e) {
-            console.log(e);
-            return reject(e);
-        }
-    });
-}
-
-async function cancelRSVP(_, { userId, eventId }, { headers, db, decodedToken }) {
-    return new Promise(async (resolve, reject) => {
-        try {
-
-            if (!db) {
-                console.log('Creating new mongoose connection.');
-                conn = await connectToMongoDB();
-            } else {
-                console.log('Using existing mongoose connection.');
-            }
-
-            /** We are findin the event from which we want to reove the user as the attendee and updating the event by removing the user 
-             * and returning the event with updated usersAttending 
-            */
-            const updatedRSVP = await Post.findOneAndUpdate({type: 'event', _id: eventId }, { $pull: { usersAttending: { $in: [ userId ] }}}, { new: true })
-                .populate({ path: 'usersAttending' }).exec();
-
-            return resolve(updatedRSVP)
-
-        } catch (e) {
-            console.log(e);
-            return reject(e);
-        }
-    });
-}
-
-async function myRSVP(_, { userId }, { headers, db, decodedToken }) {
-    return new Promise(async (resolve, reject) => {
-        try {
-
-            if (!db) {
-                console.log('Creating new mongoose connection.');
-                conn = await connectToMongoDB();
-            } else {
-                console.log('Using existing mongoose connection.');
-            }
-
-            const myRSVPEvents = await Post.find({type: 'event', 'usersAttending': { $in: [userId] } })
-                .populate({ path: 'createdBy' }).exec();
-
-            return resolve(myRSVPEvents)
-
-        } catch (e) {
-            console.log(e);
-            return reject(e);
-        }
-    });
-}
 
 async function deletePost(_, { postId }, { headers, db, decodedToken }) {
     return new Promise(async (resolve, reject) => {
@@ -381,8 +285,4 @@ module.exports = {
     getPostsByType,
     updatePost,
     deletePost,
-
-    rsvpEvent,
-    myRSVP,
-    cancelRSVP
 }
