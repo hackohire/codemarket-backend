a6b6fe615e366c9150e5d4f96efac6b060c9850e
jane <debluisle@gmail.com>
2020-04-04 18:54:46 -0700

    add homework.js mongoo model

---
+++ models/homework.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const homeworkSchema = new Schema (
+    {
+        assignmentNo: Number,
+        title: String,
+        assignmentPost: String,
+        demoUrl: String
+    },
+    {
+        timestapms: true, 
+    },
+);
+
+module.exports = () => {
+    try {
+        return mongoose.model('homework');
+    }
+    catch (e) {
+        return mongoose.model('homework', homeworkSchema);
+    }
+};
